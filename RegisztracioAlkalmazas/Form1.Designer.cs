ea8ed0fba694f11acb6843d903b3df618ad8c605
Ádám Tilly <tilly.adam@gmail.com>
2020-10-30 15:50:57 +0100

    Layout Done

--- RegisztracioAlkalmazas/Form1.Designer.cs
+++ RegisztracioAlkalmazas/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RegisztracioAlkalmazas
 {
-    partial class Form1
+    partial class RegisztracioAlkalmazas
     {
         /// <summary>
         /// Required designer variable.
@@ -28,21 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label_Nev = new System.Windows.Forms.Label();
+            this.label_SzulDatum = new System.Windows.Forms.Label();
+            this.textBox_Nev = new System.Windows.Forms.TextBox();
+            this.dateTimePicker_SzulDatum = new System.Windows.Forms.DateTimePicker();
+            this.label_Nem = new System.Windows.Forms.Label();
+            this.radioButton_Ferfi = new System.Windows.Forms.RadioButton();
+            this.radioButton_No = new System.Windows.Forms.RadioButton();
+            this.label_KedvencHobbi = new System.Windows.Forms.Label();
+            this.listBox_KedvencHobbi = new System.Windows.Forms.ListBox();
+            this.textBox_UjHobbi = new System.Windows.Forms.TextBox();
+            this.label_UjHobbi = new System.Windows.Forms.Label();
+            this.button_Hozzaad = new System.Windows.Forms.Button();
+            this.button_Betoltes = new System.Windows.Forms.Button();
+            this.button_Mentes = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // label_Nev
+            // 
+            this.label_Nev.AutoSize = true;
+            this.label_Nev.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Nev.Location = new System.Drawing.Point(40, 39);
+            this.label_Nev.Name = "label_Nev";
+            this.label_Nev.Size = new System.Drawing.Size(68, 27);
+            this.label_Nev.TabIndex = 0;
+            this.label_Nev.Text = "Név:";
+            // 
+            // label_SzulDatum
+            // 
+            this.label_SzulDatum.AutoSize = true;
+            this.label_SzulDatum.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_SzulDatum.Location = new System.Drawing.Point(40, 87);
+            this.label_SzulDatum.Name = "label_SzulDatum";
+            this.label_SzulDatum.Size = new System.Drawing.Size(180, 27);
+            this.label_SzulDatum.TabIndex = 1;
+            this.label_SzulDatum.Text = "Szül. dátum:";
+            // 
+            // textBox_Nev
+            // 
+            this.textBox_Nev.Font = new System.Drawing.Font("Courier New", 13.8F);
+            this.textBox_Nev.Location = new System.Drawing.Point(226, 36);
+            this.textBox_Nev.Name = "textBox_Nev";
+            this.textBox_Nev.Size = new System.Drawing.Size(322, 34);
+            this.textBox_Nev.TabIndex = 2;
+            // 
+            // dateTimePicker_SzulDatum
+            // 
+            this.dateTimePicker_SzulDatum.Font = new System.Drawing.Font("Courier New", 13.8F);
+            this.dateTimePicker_SzulDatum.Location = new System.Drawing.Point(226, 80);
+            this.dateTimePicker_SzulDatum.Name = "dateTimePicker_SzulDatum";
+            this.dateTimePicker_SzulDatum.Size = new System.Drawing.Size(322, 34);
+            this.dateTimePicker_SzulDatum.TabIndex = 3;
+            // 
+            // label_Nem
+            // 
+            this.label_Nem.AutoSize = true;
+            this.label_Nem.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Nem.Location = new System.Drawing.Point(40, 140);
+            this.label_Nem.Name = "label_Nem";
+            this.label_Nem.Size = new System.Drawing.Size(68, 27);
+            this.label_Nem.TabIndex = 4;
+            this.label_Nem.Text = "Nem:";
+            // 
+            // radioButton_Ferfi
+            // 
+            this.radioButton_Ferfi.AutoSize = true;
+            this.radioButton_Ferfi.Font = new System.Drawing.Font("Courier New", 13.8F);
+            this.radioButton_Ferfi.Location = new System.Drawing.Point(226, 140);
+            this.radioButton_Ferfi.Name = "radioButton_Ferfi";
+            this.radioButton_Ferfi.Size = new System.Drawing.Size(103, 31);
+            this.radioButton_Ferfi.TabIndex = 5;
+            this.radioButton_Ferfi.TabStop = true;
+            this.radioButton_Ferfi.Text = "Férfi";
+            this.radioButton_Ferfi.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_No
+            // 
+            this.radioButton_No.AutoSize = true;
+            this.radioButton_No.Font = new System.Drawing.Font("Courier New", 13.8F);
+            this.radioButton_No.Location = new System.Drawing.Point(351, 140);
+            this.radioButton_No.Name = "radioButton_No";
+            this.radioButton_No.Size = new System.Drawing.Size(61, 31);
+            this.radioButton_No.TabIndex = 6;
+            this.radioButton_No.TabStop = true;
+            this.radioButton_No.Text = "Nő";
+            this.radioButton_No.UseVisualStyleBackColor = true;
+            // 
+            // label_KedvencHobbi
+            // 
+            this.label_KedvencHobbi.AutoSize = true;
+            this.label_KedvencHobbi.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_KedvencHobbi.Location = new System.Drawing.Point(40, 198);
+            this.label_KedvencHobbi.Name = "label_KedvencHobbi";
+            this.label_KedvencHobbi.Size = new System.Drawing.Size(208, 27);
+            this.label_KedvencHobbi.TabIndex = 7;
+            this.label_KedvencHobbi.Text = "Kedvenc hobbi:";
+            // 
+            // listBox_KedvencHobbi
+            // 
+            this.listBox_KedvencHobbi.Font = new System.Drawing.Font("Courier New", 13.8F);
+            this.listBox_KedvencHobbi.FormattingEnabled = true;
+            this.listBox_KedvencHobbi.ItemHeight = 25;
+            this.listBox_KedvencHobbi.Location = new System.Drawing.Point(254, 198);
+            this.listBox_KedvencHobbi.Name = "listBox_KedvencHobbi";
+            this.listBox_KedvencHobbi.Size = new System.Drawing.Size(294, 204);
+            this.listBox_KedvencHobbi.TabIndex = 8;
+            // 
+            // textBox_UjHobbi
+            // 
+            this.textBox_UjHobbi.Font = new System.Drawing.Font("Courier New", 13.8F);
+            this.textBox_UjHobbi.Location = new System.Drawing.Point(254, 417);
+            this.textBox_UjHobbi.Name = "textBox_UjHobbi";
+            this.textBox_UjHobbi.Size = new System.Drawing.Size(294, 34);
+            this.textBox_UjHobbi.TabIndex = 10;
+            // 
+            // label_UjHobbi
+            // 
+            this.label_UjHobbi.AutoSize = true;
+            this.label_UjHobbi.Font = new System.Drawing.Font("Courier New", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_UjHobbi.Location = new System.Drawing.Point(40, 420);
+            this.label_UjHobbi.Name = "label_UjHobbi";
+            this.label_UjHobbi.Size = new System.Drawing.Size(138, 27);
+            this.label_UjHobbi.TabIndex = 9;
+            this.label_UjHobbi.Text = "Új hobbi:";
+            // 
+            // button_Hozzaad
+            // 
+            this.button_Hozzaad.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Hozzaad.Location = new System.Drawing.Point(351, 457);
+            this.button_Hozzaad.Name = "button_Hozzaad";
+            this.button_Hozzaad.Size = new System.Drawing.Size(115, 34);
+            this.button_Hozzaad.TabIndex = 11;
+            this.button_Hozzaad.Text = "Hozzáad";
+            this.button_Hozzaad.UseVisualStyleBackColor = true;
+            // 
+            // button_Betoltes
+            // 
+            this.button_Betoltes.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Betoltes.Location = new System.Drawing.Point(335, 554);
+            this.button_Betoltes.Name = "button_Betoltes";
+            this.button_Betoltes.Size = new System.Drawing.Size(115, 34);
+            this.button_Betoltes.TabIndex = 12;
+            this.button_Betoltes.Text = "Betöltés";
+            this.button_Betoltes.UseVisualStyleBackColor = true;
+            // 
+            // button_Mentes
+            // 
+            this.button_Mentes.Font = new System.Drawing.Font("Courier New", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Mentes.Location = new System.Drawing.Point(214, 554);
+            this.button_Mentes.Name = "button_Mentes";
+            this.button_Mentes.Size = new System.Drawing.Size(115, 34);
+            this.button_Mentes.TabIndex = 13;
+            this.button_Mentes.Text = "Mentés";
+            this.button_Mentes.UseVisualStyleBackColor = true;
+            // 
+            // RegisztracioAlkalmazas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(633, 535);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(633, 600);
+            this.Controls.Add(this.button_Mentes);
+            this.Controls.Add(this.button_Betoltes);
+            this.Controls.Add(this.button_Hozzaad);
+            this.Controls.Add(this.textBox_UjHobbi);
+            this.Controls.Add(this.label_UjHobbi);
+            this.Controls.Add(this.listBox_KedvencHobbi);
+            this.Controls.Add(this.label_KedvencHobbi);
+            this.Controls.Add(this.radioButton_No);
+            this.Controls.Add(this.radioButton_Ferfi);
+            this.Controls.Add(this.label_Nem);
+            this.Controls.Add(this.dateTimePicker_SzulDatum);
+            this.Controls.Add(this.textBox_Nev);
+            this.Controls.Add(this.label_SzulDatum);
+            this.Controls.Add(this.label_Nev);
+            this.Name = "RegisztracioAlkalmazas";
+            this.Text = "RegisztracioAlkalmazas";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label_Nev;
+        private System.Windows.Forms.Label label_SzulDatum;
+        private System.Windows.Forms.TextBox textBox_Nev;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_SzulDatum;
+        private System.Windows.Forms.Label label_Nem;
+        private System.Windows.Forms.RadioButton radioButton_Ferfi;
+        private System.Windows.Forms.RadioButton radioButton_No;
+        private System.Windows.Forms.Label label_KedvencHobbi;
+        private System.Windows.Forms.ListBox listBox_KedvencHobbi;
+        private System.Windows.Forms.TextBox textBox_UjHobbi;
+        private System.Windows.Forms.Label label_UjHobbi;
+        private System.Windows.Forms.Button button_Hozzaad;
+        private System.Windows.Forms.Button button_Betoltes;
+        private System.Windows.Forms.Button button_Mentes;
     }
 }
 
--- RegisztracioAlkalmazas/Form1.cs
+++ RegisztracioAlkalmazas/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RegisztracioAlkalmazas
 {
-    public partial class Form1 : Form
+    public partial class RegisztracioAlkalmazas : Form
     {
-        public Form1()
+        public RegisztracioAlkalmazas()
         {
             InitializeComponent();
         }
