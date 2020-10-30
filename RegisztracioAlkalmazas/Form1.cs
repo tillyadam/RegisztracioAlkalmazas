e77c5fd5185488c6588e61a49affdd3e98f8bdd3
Ádám Tilly <tilly.adam@gmail.com>
2020-10-30 17:26:23 +0100

    button_Mentes_Click() done

--- RegisztracioAlkalmazas/Form1.cs
+++ RegisztracioAlkalmazas/Form1.cs
@@ -16,11 +16,12 @@ namespace RegisztracioAlkalmazas
         public RegisztracioAlkalmazas()
         {
             InitializeComponent();
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            this.CenterToScreen();
         }
 
         private void button_Hozzaad_Click(object sender, EventArgs e)
@@ -36,8 +37,31 @@ namespace RegisztracioAlkalmazas
         }
 
         private void button_Mentes_Click(object sender, EventArgs e)
-        {            
-            
+        {
+            if ((!String.IsNullOrWhiteSpace(textBox_Nev.Text)) && (dateTimePicker_SzulDatum.Value.Date<=DateTime.Now.Date) && (radioButton_Ferfi.Checked || radioButton_No.Checked) && (listBox_KedvencHobbi.SelectedIndex!=-1))
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filenev = saveFileDialog.FileName;
+                    StreamWriter sw = new StreamWriter(filenev);
+                    
+                    sw.Write(textBox_Nev.Text.Trim() + ";" + dateTimePicker_SzulDatum.Value.Date + ";" + radioButton_Ferfi.Checked + ";" + radioButton_No.Checked);
+                    foreach (var item in listBox_KedvencHobbi.Items)
+                    {
+                        sw.Write("," + item);
+                    }
+
+                    sw.Close();
+                    
+                }
+            }
+            else
+            {
+                MessageBox.Show("Valami nincs rendesen kitöltve");
+            }
         }
     }
 }
